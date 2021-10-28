96fdfe66aeff7f99cf4b0b172f72bfe40a3d7111
UmamaheswaraChittem <36254815+UmamaheswaraChittem@users.noreply.github.com>
2021-10-28 12:46:46 +0530

    Update controller.js

--- Node-Web-Services/controllers/controller.js
+++ Node-Web-Services/controllers/controller.js
@@ -1,13 +1,14 @@
 const logger = require('../resources/logging.js');
 //const dbApi = require('../db_apis/dbApi.js');
 const restClient = require('../infra_services/restClient.js');
+const config = require('../config/appConfig.js');
 
 async function getUniversitiesDetails(req, res, next) {
     try {
         //const response = await dbApi.getUniversitiesDetails(req); with DB fetching
         const options = {
             method: 'GET',
-            url: `http://universities.hipolabs.com/search?country=${req.query.country}`
+            url: config.getEnvironmentConfig()+`?country=${req.query.country}`
         }
         const response = await restClient.getRequest(options);
         res.status(200).json(JSON.parse(response));
